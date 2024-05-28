0c13358c94b8f9478e6688ae9f6af82d2c8bab0d
Marcelo Fabiano <speedpaythonedu@gmail.com>
2024-05-28 12:25:20 -0300

    commit inicial

---
+++ codigospython/teste.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+# Send an Image to a Contact with the no Caption
+pywhatkit.sendwhats_image("+5521976918010", "Images/marcelofabiano.jpg", "http://www.marcelofabiano.com.br")  #sendwhats_image("+5521991638862", "Images/Huge.png")
